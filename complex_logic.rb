b3ac6e7c3687451185cb519d3cb6d69d27ca42c3
Willem A. Hajenius <w.a.hajenius@gmail.com>
2018-01-17 08:48:52 +0100

    Bugfixes

--- complex_logic.rb
+++ complex_logic.rb
@@ -1,9 +1,9 @@
-# Exercise 5: show in editor
+# Some complex logic
 
 a = 9
 b = 3
 c = 7
 
-a / b <= b
-100 % b != c % b
-a % b == c / b || (a / b) – 1 == c / b
+puts a / b <= b
+puts 100 % b != c % b
+puts a % b == c / b || (a / b) - 1 == c / b
--- hashes.rb
+++ hashes.rb
@@ -7,11 +7,17 @@ puts "Two in German is " + dictionary['two']
 dictionary['please'] = 'bitte'
 dictionary['beer'] = 'Bier'
 
+# Split sentence into an array of words. Splits on whitespace.
 english_words = "two beer please".split
+
+# Array for the translated words
 translation = []
 
 english_words.each do |word|
+    # Translate each word and add it to the array of translated words
     translation.push dictionary[word]
 end
+
+# Join the translated words together with whitespace in between
 puts translation.join ' '
 
