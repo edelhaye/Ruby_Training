867ae394f6a3189e510e108d2f4d074587934d7f
Willem A. Hajenius <w.a.hajenius@gmail.com>
2018-02-06 08:27:40 +0100

    Transformed scripts into test cases

---
+++ Session2/scenario1b.rb
@@ -0,0 +1,28 @@
+# Load libraries for Selenium Webdriver and unit tests
+require "selenium-webdriver"
+require "test/unit"
+
+class Scenario1b < Test::Unit::TestCase
+    def setup
+        @driver = Selenium::WebDriver.for :chrome
+    end
+    
+    def teardown
+        @driver.quit
+    end
+    
+    def test_the_thing
+        # Visit the site
+        @driver.navigate.to "http://automationpractice.com/index.php"
+
+        # Locate the search box and search something
+        search_box = @driver.find_element(id: 'search_query_top')
+        search_box.send_keys "dress"
+        search_box.submit
+
+        # Print how many results
+        element = @driver.find_element(css: '.heading-counter')
+        puts element.text
+        assert_equal(7, element.text.to_i)
+    end
+end
--- Session2/scenario2.rb
+++ Session2/scenario2.rb
@@ -1,35 +1,42 @@
-# Load the Ruby library for Selenium Webdriver
+# Load libraries for Selenium Webdriver and unit tests
 require "selenium-webdriver"
+require "test/unit"
 
-# Load the driver
-driver = Selenium::WebDriver.for :chrome
+class Scenario2 < Test::Unit::TestCase
+    def setup
+        @driver = Selenium::WebDriver.for :chrome
 
-# Prepare a wait function
-wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+        # Prepare a wait function
+        @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+    end
+    
+    def teardown
+        @driver.quit
+    end
+    
+    def test_the_thing
+        # Visit the site
+        @driver.navigate.to "http://automationpractice.com/index.php"
 
-# Visit the site
-driver.navigate.to "http://automationpractice.com/index.php"
+        # Locate the search box and search something
+        search_box = @driver.find_element(id: 'search_query_top')
+        search_box.send_keys "blouse"
 
-# Locate the search box and search something
-search_box = driver.find_element(id: 'search_query_top')
-search_box.send_keys "blouse"
+        search_button = @driver.find_element(css: 'button.button-search[name=submit_search]')
+        search_button.click
 
-search_button = driver.find_element(css: 'button.button-search[name=submit_search]')
-search_button.click
+        # Print how many results
+        element = @driver.find_element(css: '.heading-counter')
+        puts element.text
+        assert_equals(1, element.text.to_i)
 
-# Print how many results
-element = driver.find_element(css: '.heading-counter')
-puts element.text
-
-# Grab the More button
-more_button = wait.until {
-    driver.find_element(css: 'a.button[title=View]>span')
-}
-more_button.click
-
-# Pausing the Ruby script here to show the last page in the browser.
-# Don't let your real scripts sleep unnecessarily.
-sleep 10
-
-# That's all folks!
-driver.quit
+        # Grab the More button
+        more_button = @wait.until {
+            @driver.find_element(css: 'a.button[title=View]>span')
+        }
+        more_button.click
+        
+        # Print the value of the "Styles" property in the datasheet
+        # Verify that it is in new condition
+    end
+end
